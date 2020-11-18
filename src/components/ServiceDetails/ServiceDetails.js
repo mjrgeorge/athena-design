1a4f7a306c40fb16fe46ab752cb5d6b189d45bf4
mjrgeorge <mjrgeorge@gmail.com>
2020-11-19 02:58:32 +0600

    Services section completed

---
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './serviceDetails.css';
+
+const ServiceDetails = ({ service }) => {
+    const { image, title, details } = service;
+    return (
+        <div className="col-md-6 col-lg-3">
+            <div className="service_container mx-auto p-5">
+                <img className="mb-3" src={image} alt="service" />
+                <h5>{title}</h5>
+                <p className="text-muted">{details}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceDetails;
---
+++ src/components/Services/Services.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ServiceDetails from '../ServiceDetails/ServiceDetails';
+
+const Services = () => {
+    const services = [
+        {
+            "id": "1",
+            "image": "https://i.ibb.co/XbkCZXt/Group1.png",
+            "title": "Experience Design",
+            "details": "The point of using Lorem Ipsum is that it has a more-orless normal"
+        },
+        {
+            "id": "2",
+            "image": "https://i.ibb.co/WBTKV34/Group2.png",
+            "title": "Interface Design",
+            "details": "The point of using Lorem Ipsum is that it has a more-orless normal"
+        },
+        {
+            "id": "3",
+            "image": "https://i.ibb.co/yX2GxRd/Group3.png",
+            "title": "Prototyping",
+            "details": "The point of using Lorem Ipsum is that it has a more-orless normal"
+        },
+        {
+            "id": "4",
+            "image": "https://i.ibb.co/8DGykY2/Group4.png",
+            "title": "Illustration",
+            "details": "The point of using Lorem Ipsum is that it has a more-orless normal"
+        }
+    ]
+    return (
+        <section className="container-fluid text-center  mt-5">
+            <div className="p-3">
+                <h2>What we do</h2>
+                <p className="text-muted">Our main focus is to make the User Experience very <br /> simple and easy. Simplicity is our Strength.</p>
+            </div>
+            <div className="row">
+                {
+                    services.map(service => <ServiceDetails service={service} key={service.id}></ServiceDetails>)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default Services;
