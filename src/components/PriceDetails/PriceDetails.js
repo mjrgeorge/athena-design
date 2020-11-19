a2cc2705ea17183aec317ccd8d05381273664de0
mjrgeorge <mjrgeorge@gmail.com>
2020-11-19 06:39:08 +0600

    price section button updated

--- src/components/PriceDetails/PriceDetails.js
+++ src/components/PriceDetails/PriceDetails.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './PriceDetails.css';
 
 const PriceDetails = ({ info }) => {
-    const { price, title, home_page, inner_page, asset_file, source_file, photos, support_duration, support_time } = info;
+    const { id, price, title, home_page, inner_page, asset_file, source_file, photos, support_duration, support_time } = info;
+    const handleMouseOver = () => {
+        document.getElementById(`btn_${id}`).style.visibility = "visible";
+    }
+    const handleMouseLeave = () => {
+        document.getElementById(`btn_${id}`).style.visibility = "hidden";
+    }
     return (
-        <div className="col-lg-4">
+        <div onMouseLeave={handleMouseLeave} onMouseOver={handleMouseOver} className="col-lg-4">
             <div className="price_container shadow rounded m-5 mx-auto p-5">
                 <p className="price font-weight-bold">${price}</p>
                 <p className="text-muted  font-weight-light">{title}</p>
@@ -17,7 +23,7 @@ const PriceDetails = ({ info }) => {
                     <p>{photos}</p>
                     <p>{support_duration}</p>
                     <p>{support_time}</p>
-                    <button className="btn btn-info">Order Now</button>
+                    <button id={`btn_${id}`} className="btn btn-info">Order Now</button>
                 </div>
             </div>
         </div>
